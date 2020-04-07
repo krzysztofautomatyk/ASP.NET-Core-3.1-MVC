ed9c4d0fed08a07f20350f4f39a3c29c47c0626c
Krzysztof Woźniczak <krzysztofautomatyk@gmail.com>
2020-04-07 15:10:17 +0200

    R64 - Koniec Nawiązano połącznie z bazą danych i utworzono bazę. Dodano nowe przyciski do menu

---
+++ Dokumentacja/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace Dokumentacja
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+
+        
+    }
+}
