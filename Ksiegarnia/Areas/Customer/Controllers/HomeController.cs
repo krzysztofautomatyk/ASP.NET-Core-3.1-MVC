1bd325323c685372dff0a3d4cc3e8343f7d581bc
Krzysztof Woźniczak <krzysztofautomatyk@gmail.com>
2020-04-18 11:11:36 +0200

    R110 - Koniec Dodano widok poszczególnych produktów na stronie głównej

--- Ksiegarnia/Areas/Customer/Controllers/HomeController.cs
+++ Ksiegarnia/Areas/Customer/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Ksiegarnia.DataAccess.Repository.IRepository;
+using Ksiegarnia.Models;
 using Ksiegarnia.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -15,14 +17,18 @@ namespace Ksiegarnia.Areas.Customer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Produkt> produktList = _unitOfWork.Produkt.GetAll(includeProperties: "Kategoria,Okladka");
+            return View(produktList);
         }
 
         public IActionResult Privacy()
